0407d5513ccad3631970a515a31586508a91182e
Dan Chrostowski <dan@danchrostowski.com>
2019-03-27 00:57:43 -0500

    Some fixes.  In the process of a major refactor.

--- example.py
+++ example.py
@@ -16,42 +16,17 @@ I have multiple accounts so I parse the json to a dict which looks like this:
 }
 
 """
-# Instantiate as you see fit, just needs a string of the cookie value.
-client1 = InvestopediaSimulatorAPI(cookies['aggregate'])
-client2 = InvestopediaSimulatorAPI(cookies['default'])
+# Instantiate as you see fit, just needs a string of the UI4 cookie value.
+client = InvestopediaSimulatorAPI(cookies['default'])
 
 
-portfolio1 = client1.stock_portfolio
-portfolio2 = client2.stock_portfolio
-
-print(portfolio1.cash)
-print(portfolio2.cash)
-embed()
-
-
-print("Default (active) game: %s" % client.active_game)
-print("Portfolio total value: %s" % portfolio.total_value)
-for holding in portfolio:
-    print("\nStock symbol: %s (%s)" %
-          (holding.security.symbol, holding.security.url))
-    print("Start price: %s" % holding.start)
-    print("Current price: %s" % holding.current)
-    print("Net return: %s\n" % holding.net_return)
-
-q = client.get_quote('AAPL')
+q = client.get_quote('MSFT')
 print("quote symbol: %s" % q.symbol)
 print("quote comp name: %s" % q.name)
 print("quote market price: %s " % q.last)
 print("quote change (per share): %s " % q.change)
 print("quote change (%%): %s " % q.change_percent)
 print(q)
-
-p = client.stock_portfolio
-print("portfolio annual %% return: %s" % p.annual_return_pct)
-print("cash: %s" % p.cash)
-print("buying power: %s" % p.buying_power)
-
-picks = ['GOOG', 'AAPL', 'AMZN']
 
 # How to do an option chain lookup
 option_lookup_symbol = 'TEO'
@@ -83,32 +58,12 @@ print("strike price for second put option in chain: %s" %
       put_option2.strike_price)
 print("expiration for second put option in chain: %s" % put_option2.expiration)
 
-trades = []
-for pick in picks:
-    stocks = portfolio.find_by_symbol(pick)
-    if len(stocks) > 0:
-        print("already own or have an order in for %s" % stocks[0].security)
-    else:
-        quote = client.get_quote(pick)
-        if quote:
-            qty_shares_to_buy = int(3200 / quote.last)
-            trade = StockTrade(
-                stock=quote,
-                quantity=qty_shares_to_buy,
-                transaction_type=TransactionType.BUY(),
-                order_type=OrderType.MARKET(),
-                order_duration=OrderDuration.GOOD_TILL_CANCELLED(),
-                sendEmail=True
-            )
 
-            trades.append(trade)
 
-        else:
-            print("Couldn't find %s" % pick)
 
 # Trade can also take strings ()
-trade2 = StockTrade(
-    stock='GRMN',
+trade1 = StockTrade(
+    stock='GOOG',
     quantity=10,
     transaction_type='buy',
     order_type='market',
@@ -116,45 +71,49 @@ trade2 = StockTrade(
     sendEmail=True
 )
 
-# This is to inteintionally trigger a TradeExceedsMaxSharesException which gets handled below
+# These can also be changed prior to callling the validate() method
+trade1.quantity  = 20
+trade1.transaction_type = 'sell_short'
+trade1.order_duration = 'day_order'
+trade1.order_type = OrderType.MARKET()
+
+validated1 = trade1.validate()
+print(validated1)
+validated1.execute()
+
+quote = client.get_quote('AAPL')
+qty_shares_to_buy = int(3200 / quote.last)
+# can also pass in a Quote object to more easily calculate how many shares you want to trade
+trade2 = StockTrade(
+    stock=quote, 
+    quantity=10,
+    # StockTrade can also take special object instances defined in stock_trade (see stock_trade.py)
+    transaction_type=TransactionType.BUY(),
+    order_type=OrderType.MARKET(),
+    order_duration=OrderDuration.GOOD_TILL_CANCELLED(),
+    sendEmail=True
+)
+
+validated2 = trade2.validate()
+validated2.execute()
+
+# This is to inteintionally trigger a TradeExceedsMaxSharesException (because simulator rules) which can be handled during the invocation of validate()
 trade3 = StockTrade(
-    stock='CYBR',
+    stock='AMZN',
     quantity=9999999999999999999,
     transaction_type='buy',
     order_type='market',
     order_duration='good_till_cancelled',
     sendEmail=True
 )
+validated3 = None
+try:
+    validated3 = trade3.validate()
+except TradeExceedsMaxSharesException as e:
+    if e.max_shares > 0:
+        trade3.quantity = e.max_shares
+        print("trade3 set to %s shares" % trade3.quantity)
+        validated3 = trade3.validate()
+        print(validated3)
+        validated3.execute()
 
-# Can change the trade like so:
-
-trade2.transaction_type = 'sell_short'
-# alternatively can do this:
-# trade2.transaction_type = TransactionType.SELL_SHORT()
-
-# modifying the order type with a string is not supported because order types can get complicated
-trade2.order_type = OrderType.LIMIT(10.61)
-
-trade2.order_duration = 'day_order'
-# can also do this:
-# trade2.order_duration = OrderDuration.DAY_ORDER()
-
-trades.append(trade2)
-trades.append(trade3)
-prepped_trades = []
-
-for trade in trades:
-    print("------------------------------\npreparing:")
-    try:
-        validated = trade.validate()
-    except TradeExceedsMaxSharesException as max_shares_error:
-        max_shares = max_shares_error.max_shares
-        if max_shares > 0:
-            print("Adjusting trade for %s." % trade.symbol)
-            trade.quantity = max_shares_error.max_shares
-            validated_trade = trade.validate()
-
-    print(validated)
-    validated.execute()
-    # uncomment to execute the trade
-    # validated.execute()
