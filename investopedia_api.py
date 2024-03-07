734029a9504fef21295222e0eb669cc405d34899
Dan Chrostowski <dan@danchrostowski.com>
2024-03-07 15:17:09 -0700

    refactoring

--- investopedia_api.py
+++ investopedia_api.py
@@ -1,9 +1,6 @@
 from parsers import Parsers, stock_quote
-from trade_common import Expiration, OrderLimit, TransactionType, Trade, StockTrade
-from trade_common import TradeNotValidatedException, InvalidOrderDurationException, InvalidOrderTypeException, InvalidTradeTypeException
+from trade_common import Expiration, OrderLimit, TransactionType, OptionTrade, StockTrade
 from options import OptionChain
-from option_trade import OptionTrade
-# from stock_trade import StockTrade
 from session_singleton import Session
 from utils import TaskQueue, validate_and_execute_trade
 import warnings
